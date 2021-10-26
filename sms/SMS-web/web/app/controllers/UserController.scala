114b37518a81160bc647903abdbcca9af1ce9ea1
vitorcodesalittle <vlma@cin.ufpe.br>
2021-10-26 20:07:00 -0300

    user marshall

--- sms/SMS-web/web/app/controllers/UserController.scala
+++ sms/SMS-web/web/app/controllers/UserController.scala
@@ -1,27 +1,28 @@
 package controllers
 
 import domain.users.{User, UserRepositoryBDR}
+import play.api.libs.json.Json
 
 import scala.concurrent.{Await, ExecutionContext, Future}
 import play.api.mvc._
 
 import javax.inject.{Inject, Singleton}
 import scala.concurrent.duration.Duration
-import scala.util.{Failure, Success}
 
 @Singleton
 class UserController @Inject() (val repo: UserRepositoryBDR, val controllerComponents: ControllerComponents)
   (implicit ec: ExecutionContext) extends BaseController {
 
-  def login() = Action {
-    val user = User(id = None, email = "vitormaia1890@gmail.com", name = "vitor maia", passwordHash = Some("asdasdasd"), passwordSalt = Some("asdasdasd"))
-    println(f"Let's try to insert $user")
-    val savedUser = Await.result(repo.create(user), Duration.Inf)
-    Ok(f"$savedUser")
+  implicit val userWrites = Json.writes[User]
+
+  def login(): Action[AnyContent] = Action {
+    NotImplemented("Sorry")
   }
 
   def signUp() = Action {
-    Ok("ok")
+    val user = User(id = None, email = "vitormaia1890@gmail.com", name = "vitor maia", passwordHash = Some("asdasdasd"), passwordSalt = Some("asdasdasd"))
+    val savedUser = Await.result(repo.create(user), Duration.Inf) // @TODO: Duration.Inf ???
+    Ok(Json.toJson(savedUser))
   }
 
   def verifyPassword(user: User, password: String): Future[Boolean] = ???
