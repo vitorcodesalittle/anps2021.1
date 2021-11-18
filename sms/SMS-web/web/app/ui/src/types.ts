0b7a9e90a74fd0f6532070639e4f9d63004e0cd2
vitor <vlma@cin.ufpe.br>
2021-11-18 20:45:41 -0300

    add types

---
+++ sms/SMS-web/web/app/ui/src/types.ts
@@ -0,0 +1,41 @@
+
+type Store = {
+  name: string
+}
+
+type User = {
+  id: number
+  name: string
+  stores: Store[]
+  email: string
+  emailVerified: boolean
+}
+
+
+type Product = {
+  id: number,
+  name: string,
+  stock: number,
+  suggestedPrice: number,
+}
+
+type Item = {
+  productId: number
+  quantity: number
+  price: number
+  product: Product
+}
+
+type Transaction = {
+  id: number
+  createdAt: Date
+  items: Item[]
+}
+
+type DeliveryMethod = "correios" | "jadlog" | "motoboy" | "sedex"
+
+type Sale = {
+  transaction: Transaction,
+  deliveryMethod: DeliveryMethod
+  deliveryPrice: number
+}
