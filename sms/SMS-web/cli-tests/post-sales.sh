f7934eb1c8c6386076c88e6e30265535018e8c0f
vitor <vlma@cin.ufpe.br>
2021-12-05 15:19:31 -0300

    rename project and create ui Dockerfile

--- sms/SMS-web/cli-tests/post-sales.sh
+++
@@ -1,2 +0,0 @@
-curl http://localhost:9000/sales -H "Content-Type: application/json" -v -H "Cookie: authtoken=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2MzYxMzgzNTcsInVzZXIiOnsiaWQiOjEsInN0b3JlSWQiOjF9fQ.upSMTzowIbUhbLluIT-FFiskjAWPCq23WNFNJ29P5FM" --data "@./saleData.json"
-
