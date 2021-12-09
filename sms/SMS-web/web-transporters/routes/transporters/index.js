7f78dcf9bb674e3f0eb3902188aa50350b186c5e
vitorcodesalittle <vlma@cin.ufpe.br>
2021-12-09 16:09:28 -0300

    update web-transporters

--- sms/SMS-web/web-transporters/routes/transporters/index.js
+++ sms/SMS-web/web-transporters/routes/transporters/index.js
@@ -3,7 +3,13 @@ const correios = require('../../transporters/correios')
 
 module.exports = async function (fastify, opts) {
   fastify.get('/correios', async function (request, reply) {
-    const response = await correios.getPreco('51130320', '04870470')
+    const result = await correios.getPreco('51130320', '04870470')
+    const correiosResponse = result[0]
+    const { valor, prazoEntrega } = correiosResponse
+    const price = parseFloat(valor.replace(/,/g, '.'))
+    const deadline = new Date()
+    deadline.setDate(deadline.getDate() + parseInt(prazoEntrega))
+    const response = { price, deadline}
     return response
   })
 }
