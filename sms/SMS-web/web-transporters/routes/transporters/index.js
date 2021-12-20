d962c5fce9e688da64741294ddfba65f3572ea80
vitor <vlma@cin.ufpe.br>
2021-12-20 08:25:22 -0300

    fix transporters

--- sms/SMS-web/web-transporters/routes/transporters/index.js
+++ sms/SMS-web/web-transporters/routes/transporters/index.js
@@ -1,15 +1,31 @@
 'use strict'
 const correios = require('../../transporters/correios')
 
+
 module.exports = async function (fastify, opts) {
-  fastify.get('/correios', async function (request, reply) {
-    const result = await correios.getPreco('51130320', '04870470')
-    const correiosResponse = result[0]
-    const { valor, prazoEntrega } = correiosResponse
-    const price = parseFloat(valor.replace(/,/g, '.'))
-    const deadline = new Date()
-    deadline.setDate(deadline.getDate() + parseInt(prazoEntrega))
-    const response = { price, deadline}
-    return response
-  })
+	const handler = async function (request, reply) {
+		console.log(request.query)
+		const cep = request.query.cep
+		const result = await correios.getPreco('51130320', cep)
+		console.log({cep})
+		const correiosResponse = result[0]
+		const { valor, prazoEntrega } = correiosResponse
+		const price = parseFloat(valor.replace(/,/g, '.'))
+		const deadline = new Date()
+		deadline.setDate(deadline.getDate() + parseInt(prazoEntrega))
+		const response = { price, deadline}
+		return response
+	}
+	return fastify.route({
+		method: 'GET',
+		url: '/correios',
+		schema: {
+			query: {
+				cep: {
+					type: "string"
+				}
+			}
+		},
+		handler
+	})
 }
